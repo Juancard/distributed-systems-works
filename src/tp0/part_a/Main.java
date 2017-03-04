5eb6b64f5c6f6879d2e12cd8d76173d1f9ec82a4
juan <juancruz.23@hotmail.com>
2017-03-04 19:28:17 -0300

    tp0: parte a

---
+++ src/tp0/part_a/Main.java
@@ -0,0 +1,108 @@
+package tp0.part_a;
+
+import java.util.Scanner;
+
+public class Main {
+
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) throws DenominadorCero {
+		Fraccion f1 = new Fraccion();
+		Fraccion f2 = new Fraccion();
+
+		String opcion;
+		boolean salir = false;
+		showMenuHeader();
+
+		while (!salir){
+			showMenu();
+			opcion = sc.nextLine();
+			switch(opcion){
+			case "1":
+				f1 = ingresarFraccion();
+				pause();
+				break;
+			case "2":
+				f2 = ingresarFraccion();
+				pause();
+				break;
+			case "3":
+				System.out.println(f1.toString() + " + " + f2.toString() + " = " + f1.sumar(f2));
+				pause();
+				break;
+			case "4":
+				System.out.println(f1.toString() + " - " + f2.toString() + " = " + f1.restar(f2));
+				pause();
+				break;
+			case "5":
+				int comparison = f1.compareTo(f2);
+				String comparisonString;
+				
+				if (comparison > 0){
+					comparisonString = " es mayor que ";
+				} else if (comparison < 0){
+					comparisonString = " es menor que ";
+				} else {
+					comparisonString = " es igual a ";
+				}
+				
+				System.out.println(f1.toString() + comparisonString + f2.toString());
+				pause();
+				break;
+			case "0":
+				salir=true;
+				break;
+			default:
+				System.out.println("Opción no válida");
+				pause();
+				break;
+			}
+		}
+		System.out.println("Adios!");
+	}
+
+	private static Fraccion ingresarFraccion() {
+		Fraccion f = new Fraccion();
+		
+		try{
+			
+			System.out.println("Numerador: ");
+			f.setNumerador(Integer.parseInt(sc.nextLine()));
+			
+			System.out.println("Denominador: ");
+			f.setDenominador(Integer.parseInt(sc.nextLine()));
+			
+		} catch (NumberFormatException e){
+			System.out.println("Valor ingresado no es un entero válido");
+			
+		} catch (DenominadorCero e){
+			System.out.println("Denominador no puede ser cero");
+		} 
+				
+		return f;
+	}
+
+	private static void pause() {
+		System.out.println("\nPresione cualquier tecla para continuar");
+		sc.nextLine();		
+	}
+
+	private static void showMenuHeader() {
+		System.out.println("*******************************************************");
+		System.out.println("Sistemas Distribuidos y Programación Paralela");
+		System.out.println("TP N°0 - Parte A");
+		System.out.println("*******************************************************\n");		
+	}
+
+	private static void showMenu(){
+		System.out.println("Fracción - Menú");
+		System.out.println("1 - Ingresar fracción I");
+		System.out.println("2 - Ingresar fracción II");
+		System.out.println("3 - Sumar");
+		System.out.println("4 - Restar");
+		System.out.println("5 - Comparar");
+		System.out.println("0 - Salir");
+		System.out.println("Ingrese opción: ...");
+	}
+	
+}

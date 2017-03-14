8c1451b5aa559c40830003bb369e68a0b3bb64a6
juan <juancruz.23@hotmail.com>
2017-03-13 23:27:14 -0300

    fucking up

--- src/tp0/A/DenominadorCero.java
+++
@@ -1,8 +0,0 @@
-package tp0.A;
-
-public class DenominadorCero extends Exception{
-  public DenominadorCero() { super(); }
-  public DenominadorCero(String message) { super(message); }
-  public DenominadorCero(String message, Throwable cause) { super(message, cause); }
-  public DenominadorCero(Throwable cause) { super(cause); }
-}

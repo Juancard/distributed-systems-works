a55c4f53824b6e1a1217606dbe896f0e0612cb1d
juan <juancruz.23@hotmail.com>
2017-05-07 16:16:39 -0300

    When running server, user can ser the period of time to wait until updating time

--- Tp2/Ex06/Server/MainServer.java
+++ Tp2/Ex06/Server/MainServer.java
@@ -3,6 +3,7 @@ package Tp2.Ex06.Server;
 import Common.CommonMain;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * User: juan
@@ -18,8 +19,27 @@ public class MainServer {
     public static void main(String[] args) {
         CommonMain.showWelcomeMessage(TP_NUMBER, EXERCISE_NUMBER, TP_TITLE);
         int port = CommonMain.askForPort(DEFAULT_PORT);
-        int updatePeriod = 6000;
+        int updatePeriod = askForUpdatePeriod();
         TimeServer timeServer = new TimeServer(port, updatePeriod);
         timeServer.startServer();
+    }
+
+    private static int askForUpdatePeriod() {
+        Scanner sc = new Scanner(System.in);
+        int period = -1;
+
+        do {
+            System.out.print("Enter seconds to wait until updating the time: ");
+            String periodString = sc.nextLine();
+            try {
+                period = Integer.parseInt(periodString);
+                if (period <= 0)
+                    CommonMain.display("Value can not be equal or less than zero");
+            } catch (NumberFormatException e){
+                CommonMain.display("Not a valid value.");
+            }
+        } while(period <= 0);
+
+        return period * 1000;
     }
 }

4d825fae066fc37716eee1a085d6e3622fb7919c
juan <juancruz.23@hotmail.com>
2017-05-08 18:21:11 -0300

    Tp2.ex07: client can post, get, dir and del any file. no authentication yet

--- Tp2/Ex07/Client/ClientMain.java
+++ Tp2/Ex07/Client/ClientMain.java
@@ -26,10 +26,12 @@ public class ClientMain {
 
     private Properties properties;
     private FileClient fileClient;
+    private Scanner scanner;
 
     public ClientMain(){
         try {
             this.properties = PropertiesManager.loadProperties(ClientMain.PROPERTIES_PATH);
+            this.scanner = new Scanner(System.in);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -38,7 +40,8 @@ public class ClientMain {
     private void start() {
         this.connectToServer();
         CommonMain.showWelcomeMessage(this.properties);
-        this.showLogin();
+        //this.showLogin();
+        this.handleMainOptions();
     }
 
     private void connectToServer() {
@@ -52,11 +55,10 @@ public class ClientMain {
         boolean isLogged = false;
 
         while (!isLogged){
-            Scanner scanner = new Scanner(System.in);
             System.out.print("Username: ");
-            String username = scanner.nextLine();
+            String username = this.scanner.nextLine();
             System.out.print("Password: ");
-            String password = scanner.nextLine();
+            String password = this.scanner.nextLine();
             try {
                 this.fileClient.login(username, password);
             } catch (LoginException e) {
@@ -65,6 +67,84 @@ public class ClientMain {
         }
 
     }
+
+    private void handleMainOptions() {
+        String opcion;
+        boolean salir = false;
+
+        while (!salir) {
+            showMain();
+            opcion = this.scanner.nextLine();
+            if (opcion.equals("0")) {
+                salir = true;
+            } else if (opcion.equals("1")){
+                CommonMain.createSection("List files");
+                handleFilesAvailable();
+                CommonMain.pause();
+            } else if (opcion.equals("2")){
+                CommonMain.createSection("Get a file");
+                handleGetFile();
+                CommonMain.pause();
+            } else if (opcion.equals("3")){
+                CommonMain.createSection("Post a file");
+                handlePostFile();
+                CommonMain.pause();
+            } else if (opcion.equals("4")){
+                CommonMain.createSection("Delete a file");
+                handleDelFile();
+                CommonMain.pause();
+            }
+        }
+        this.fileClient.close();
+    }
+
+    private void handleFilesAvailable() {
+        String[] filesAvailable = this.fileClient.dir();
+        for (String fileName : filesAvailable)
+            CommonMain.display(fileName);
+    }
+
+    private void handleGetFile() {
+        System.out.print("Enter File Name: ");
+        String fileName = this.scanner.nextLine();
+        String fileContent = this.fileClient.get(fileName);
+
+        CommonMain.display("File name: " + fileName);
+        CommonMain.display("Content: " + fileContent);
+    }
+
+    private void handlePostFile() {
+        System.out.print("Enter File Name: ");
+        String fileName = this.scanner.nextLine();
+        System.out.print("Enter File content: ");
+        String fileContent = this.scanner.nextLine();
+        if (this.fileClient.post(fileName, fileContent))
+            CommonMain.display("File successfully created");
+        else
+            CommonMain.display("File could not be added. Try again later.");
+    }
+
+    private void handleDelFile() {
+        System.out.print("Enter File Name: ");
+        String fileName = this.scanner.nextLine();
+
+        if (this.fileClient.del(fileName))
+            CommonMain.display("File successfully deleted");
+        else
+            CommonMain.display("File could not be deleted. Try again later.");
+    }
+
+    public void showMain(){
+        CommonMain.createSection("Main");
+        System.out.println("1 - List files");
+        System.out.println("2 - Get a file");
+        System.out.println("3 - Post a file");
+        System.out.println("4 - Delete a file");
+        System.out.println("0 - Salir");
+        System.out.println("");
+        System.out.print("Ingrese opción: ");
+    }
+
 
 
 }
